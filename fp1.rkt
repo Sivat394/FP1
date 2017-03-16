e0cd18834f17c85a215b99a7cd56f2420fc15014
Sivat394 <Victor@sivat.io>
2017-03-15 20:03:24 -0400

    Add files via upload

--- fp1.rkt
+++ fp1.rkt
@@ -4,15 +4,7 @@
 (require web-server/http/request-structs)
 (require web-server/http/bindings)
 (require json)
-(require net/url)
 
-(define blueline (string->url "https://www.uml.edu/api/Transportation/RoadsterRoutes/BusesOnLine/?apiKey=87C6ACB0-C2A4-460A-AAF2-9493BAA3917B&lineId=1"))
-(define redline  (string->url "https://www.uml.edu/api/Transportation/RoadsterRoutes/BusesOnLine/?apiKey=87C6ACB0-C2A4-460A-AAF2-9493BAA3917B&lineId=2"))
-(define redport (get-pure-port redline))
-(define blueport (get-pure-port blueline))
-
-(newline)
-(define redJSON (bytes->string/utf-8 (port->bytes redport)))
 
 (define (requestJSON req) (bytes->string/utf-8 (request-post-data/raw req)))
 
@@ -20,7 +12,7 @@
 
 (define (post_response req) (if (equal? (check_content req) "application/json")
                                 (jsexpr->string (requestJSON req))
-                                (jsexpr->string redJSON)
+                                "lol not json"
                                 )
   )
 
